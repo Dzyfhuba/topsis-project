f3fb4bbaf41ced43b23943969af54dbba2828512
Dzyfhuba <hafidz21ub@gmail.com>
2022-07-13 22:24:25 +0700

    add handsontable

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,34 +1,71 @@
 import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, /*useState*/ } from 'react'
 import Navbar from '../Containers/Navbar'
-import { HotTable } from '@handsontable/react'
+// import { HotTable } from '@handsontable/react'
 import { registerAllModules } from 'handsontable/registry'
+import Handsontable from 'handsontable'
 
 registerAllModules();
 
 const Home = () => {
-	const [data, setData] = useState([])
-	const [colHeaders, setColHeaders] = useState([])
+	// const [data, setData] = useState([])
+	// const [colHeaders, setColHeaders] = useState([])
+	// const [hot, setHot] = useState(Object)
 
 	useEffect(() => {
-		axios.get('http://localhost:3333/students').then(response => parseDataToTable(response.data.students))
+		axios.get('http://localhost:3333/students')
+		.then(response => parseDataToTable(response.data.students))
+		.then(({parsedArray, keys}) => {
+			console.log(parsedArray)
+			const container = document.querySelector('#data')
+			const hot = new Handsontable(container, {
+				data: parsedArray,
+				colHeaders: keys,
+				height: '80vh',
+				rowHeaders: true,
+				columnSorting: true,
+				dropdownMenu: true,
+				filters: true,
+				licenseKey: 'non-commercial-and-evaluation'
+			})
+			hot.render()
+		})
 	}, [])
 
 	const parseDataToTable = async (data) => {
 		const parsed = await data.map(item => {
-			return [
-				item.name,
-				item.department,
-				item.year,
-				item.joined ? 'Gabung' : 'Tidak',
-				item.status,
-				item.parents_income,
-				item.parents_expense
-			]
+			return {
+				name: item.name,
+				department: item.department,
+				year: item.year,
+				joined: item.joined ? 'Gabung' : 'Tidak',
+				status: item.status,
+				parents_income: item.parents_income,
+				parents_expense: item.parents_expense
+			}
 		})
-		// parsed.pre
-		setData(parsed)
-		setColHeaders(Object.keys(data[0]))
+		console.log(parsed)
+
+		const parsedArray = parsed.map(({
+			name,
+			department,
+			year,
+			joined,
+			status,
+			parents_income,
+			parents_expense,
+		}) => [
+			name,
+			department,
+			year,
+			joined,
+			status,
+			parents_income,
+			parents_expense,
+		])
+		console.log(parsedArray)
+		const keys = Object.keys(parsed[0])
+		return {parsedArray, keys}
 	}
 
 	return (
@@ -36,14 +73,8 @@ const Home = () => {
 			<header>
 				<Navbar />
 			</header>
-			<main className='min-h-screen py-20 bg-primary'>
-				<HotTable
-				data={data}
-				colHeaders={colHeaders}
-				width='600'
-		        height='300'
-
-				/>
+			<main className='min-h-screen py-20 bg-primary flex justify-center'>
+				<div id='data'></div>
 			</main>
 			
 		</>

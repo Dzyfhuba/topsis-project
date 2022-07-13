9c7139e197d0fb2c63bec4b9d332f453c35e3cf8
Dzyfhuba <hafidz21ub@gmail.com>
2022-07-13 22:53:41 +0700

    add export button

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,16 +1,17 @@
 import axios from 'axios'
-import React, { useEffect, /*useState*/ } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../Containers/Navbar'
 // import { HotTable } from '@handsontable/react'
 import { registerAllModules } from 'handsontable/registry'
 import Handsontable from 'handsontable'
+import Button from '../Components/Button'
 
 registerAllModules();
 
 const Home = () => {
 	// const [data, setData] = useState([])
 	// const [colHeaders, setColHeaders] = useState([])
-	// const [hot, setHot] = useState(Object)
+	const [hot, setHot] = useState(Object)
 
 	useEffect(() => {
 		axios.get('http://localhost:3333/students')
@@ -18,17 +19,18 @@ const Home = () => {
 		.then(({parsedArray, keys}) => {
 			console.log(parsedArray)
 			const container = document.querySelector('#data')
-			const hot = new Handsontable(container, {
+			const hot_elem = new Handsontable(container, {
 				data: parsedArray,
 				colHeaders: keys,
 				height: '80vh',
+				width: '80vw',
 				rowHeaders: true,
 				columnSorting: true,
 				dropdownMenu: true,
 				filters: true,
 				licenseKey: 'non-commercial-and-evaluation'
 			})
-			hot.render()
+			setHot(hot_elem)
 		})
 	}, [])
 
@@ -68,12 +70,32 @@ const Home = () => {
 		return {parsedArray, keys}
 	}
 
+	const handleExport = () => {
+		const exportPlugin = hot.getPlugin('exportFile');
+
+		exportPlugin.downloadFile('csv', {
+			bom: false,
+			columnDelimiter: ',',
+			columnHeaders: false,
+			exportHiddenColumns: true,
+			exportHiddenRows: true,
+			fileExtension: 'csv',
+			filename: 'Handsontable-CSV-file_[YYYY]-[MM]-[DD]',
+			mimeType: 'text/csv',
+			rowDelimiter: '\r\n',
+			rowHeaders: true
+		  });	
+	}
+
 	return (
 		<>
 			<header>
 				<Navbar />
 			</header>
-			<main className='min-h-screen py-20 bg-primary flex justify-center'>
+			<main className='min-h-screen py-20 bg-primary flex flex-col items-center justify-center'>
+				<div className="mb-3">
+					<Button onClick={handleExport}>Export and Download</Button>
+				</div>
 				<div id='data'></div>
 			</main>
 			
